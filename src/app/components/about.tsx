a2cf8d957f94eca040aa2f5b10bd4513a505ad69
grahamusai <grahamusai0@gmail.com>
2024-05-23 12:52:32 +0200

    responsive upadte

--- src/app/components/about.tsx
+++ src/app/components/about.tsx
@@ -15,7 +15,7 @@ const About = () => {
         deserunt eveniet pariatur itaque rerum fugit fuga.
       </p>
 
-      <div className="flex mx-24 text-white text-center mt-5 gap-10">
+      <div className="flex flex-col md:flex-row mx-5 md:mx-24 text-white text-center mt-5 gap-10">
         <div className="w-full md:w-1/2 bg-blue-gray-900 px-12 py-16 rounded-lg">
           <h2 className="">Presentation</h2>
           <h3 className="text-2xl font-bold mb-3">Lorem Ipsum Dolor</h3>
@@ -39,10 +39,10 @@ const About = () => {
           </button>
         </div>
       </div>
-      <div className="max-w-auto mx-24 text-white my-12 flex flex-col justify-center items-center bg-blue-gray-900 px-12 py-[6rem] rounded-lg">
+      <div className="max-w-auto mx-10 md:mx-24 text-white my-12 flex flex-col justify-center items-center bg-blue-gray-900 px-12 py-[6rem] rounded-lg">
         <h2 className="">Community</h2>
         <h3 className="text-2xl font-bold mb-3">Student Networking Events</h3>
-        <p className="px-10">
+        <p className="px-10 text-center">
           Gain deep insights into the latest AI trends, developments, and
           applications that are reshaping industries worldwide
         </p>
